2110d12474875cdddd639093dd2b47c872960332
Shilpa Suresh <shilpaskumar95@gmail.com>
2025-11-17 17:00:51 -0800

    Add Lambda function for media processing with IAM roles
    This Terraform configuration sets up a Lambda function for media processing, including IAM roles, security groups, and CloudWatch alarms.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,1 +1,224 @@
 
+# terraform/lambda.tf
+# Lambda function for media processing
+
+# Lambda IAM Role
+resource "aws_iam_role" "lambda_media_processing" {
+  name = "${var.project_name}-lambda-media-processing"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_media_processing" {
+  name = "${var.project_name}-lambda-media-processing-policy"
+  role = aws_iam_role.lambda_media_processing.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "${aws_s3_bucket.media.arn}/*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ]
+        Resource = aws_sqs_queue.media_processing.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "rds:DescribeDBInstances",
+          "rds:DescribeDBClusters"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Security group for Lambda
+resource "aws_security_group" "lambda" {
+  name        = "${var.project_name}-lambda-sg"
+  description = "Security group for Lambda functions"
+  vpc_id      = aws_vpc.main.id
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-lambda-sg"
+  }
+}
+
+# Allow Lambda to access RDS
+resource "aws_security_group_rule" "lambda_to_db" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.lambda.id
+  security_group_id        = aws_security_group.db.id
+}
+
+# Allow Lambda to access Redis
+resource "aws_security_group_rule" "lambda_to_cache" {
+  type                     = "ingress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.lambda.id
+  security_group_id        = aws_security_group.cache.id
+}
+
+# Lambda function
+resource "aws_lambda_function" "media_processing" {
+  filename      = "lambda_function.zip"
+  function_name = "${var.project_name}-media-processing"
+  role          = aws_iam_role.lambda_media_processing.arn
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+  timeout       = 300
+  memory_size   = 1024
+
+  vpc_config {
+    subnet_ids         = aws_subnet.private[*].id
+    security_group_ids = [aws_security_group.lambda.id]
+  }
+
+  environment {
+    variables = {
+      S3_MEDIA_BUCKET = aws_s3_bucket.media.id
+      DB_HOST         = aws_db_instance.main.address
+      DB_PORT         = "5432"
+      DB_NAME         = "mediaplatform"
+      DB_USER         = var.db_username
+      DB_PASSWORD     = var.db_password
+      REDIS_HOST      = aws_elasticache_cluster.main.cache_nodes[0].address
+      REDIS_PORT      = "6379"
+    }
+  }
+
+  reserved_concurrent_executions = 10
+
+  tags = {
+    Name = "${var.project_name}-media-processing"
+  }
+}
+
+# SQS trigger for Lambda
+resource "aws_lambda_event_source_mapping" "sqs_trigger" {
+  event_source_arn = aws_sqs_queue.media_processing.arn
+  function_name    = aws_lambda_function.media_processing.arn
+  batch_size       = 10
+  enabled          = true
+
+  scaling_config {
+    maximum_concurrency = 10
+  }
+}
+
+# CloudWatch Log Group
+resource "aws_cloudwatch_log_group" "lambda_media_processing" {
+  name              = "/aws/lambda/${aws_lambda_function.media_processing.function_name}"
+  retention_in_days = 7
+
+  tags = {
+    Name = "${var.project_name}-lambda-logs"
+  }
+}
+
+# CloudWatch Alarms for Lambda
+resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
+  alarm_name          = "${var.project_name}-lambda-errors"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = 5
+  alarm_description   = "This metric monitors lambda errors"
+
+  dimensions = {
+    FunctionName = aws_lambda_function.media_processing.function_name
+  }
+
+  alarm_actions = [aws_sns_topic.alerts.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda_throttles" {
+  alarm_name          = "${var.project_name}-lambda-throttles"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Throttles"
+  namespace           = "AWS/Lambda"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = 1
+  alarm_description   = "This metric monitors lambda throttles"
+
+  dimensions = {
+    FunctionName = aws_lambda_function.media_processing.function_name
+  }
+
+  alarm_actions = [aws_sns_topic.alerts.arn]
+}
+
+# SNS Topic for Alerts
+resource "aws_sns_topic" "alerts" {
+  name = "${var.project_name}-alerts"
+
+  tags = {
+    Name = "${var.project_name}-alerts"
+  }
+}
+
+resource "aws_sns_topic_subscription" "alerts_email" {
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "email"
+  endpoint  = var.alert_email
+}
