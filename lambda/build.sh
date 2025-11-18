cbe9c4e73eee5c4179812e3c509b0917a30156f2
Shilpa Suresh <shilpaskumar95@gmail.com>
2025-11-17 16:49:44 -0800

    Add build script for Lambda deployment package

--- lambda/build.sh
+++ lambda/build.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+set -e
 
+echo "Building Lambda deployment package..."
+
+# Install dependencies
+npm install --production
+
+# Create deployment package
+zip -r lambda_function.zip index.js node_modules/
+
+# Copy to terraform directory
+cp lambda_function.zip ../terraform/
+
+echo "Lambda package built successfully: lambda_function.zip"
+echo "Package size: $(du -h lambda_function.zip | cut -f1)"
